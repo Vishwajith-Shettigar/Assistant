b640b461bdb137ec66f9fe02b4534b59b7136f82
Vishwajith Shettigar <76042077+Vishwajith-Shettigar@users.noreply.github.com>
2021-11-20 11:59:27 +0530

    upgraded

--- jarvis.py
+++ jarvis.py
@@ -5,13 +5,14 @@ import wikipedia
 import webbrowser
 import random
 import smtplib
+from playsound import playsound
 # import pyaudio
-import os
+import os 
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 print(voices[2])
-engine.setProperty('voice',voices[1].id)
+engine.setProperty('voice',voices[0].id)
 
 def speak(audio):
     engine.say(audio)
@@ -20,6 +21,7 @@ def speak(audio):
 def wish():
     hour=datetime.datetime.now().hour
     print(hour)
+    
     if hour>0 and hour<12:
         speak("Good morning  ")
     elif hour>=12 and hour<17:
@@ -28,9 +30,36 @@ def wish():
         speak("Good evening  ")
     minutes=datetime.datetime.now().minute
     print(minutes)
-    speak(f" i am lady jarvis ,     time is {hour}, {minutes}")
+    if hour>12:
+        hour-=12
+
+        speak(f" i am lady jarvis ,     time is {hour}, {minutes} PM")
+    else:
+        speak(f" i am lady jarvis ,     time is {hour}, {minutes} AM")
+        
     speak("   How can i help you ?")
+
+def first():
     
+    r=sr.Recognizer()
+    # print(sr.__version__)
+    with sr.Microphone() as source:
+        print("Listening....")
+        r.pause_threshold=0.7
+        r.energy_threshold =300
+        audioe=r.listen(source)  
+        
+        
+    try:
+        print("Recognising....")
+        query=r.recognize_google(audioe,language='en-in')
+        print(f"user said : {query}")
+    except Exception as e:     
+        print("Say again..")
+        return "none"
+        
+    return query 
+
         
 
 def takecommand():
@@ -67,65 +96,85 @@ def sendemail(to,ac,content):
     
     
     
+def meme():
+  
+        playsound("D:\\Jarvis_assistant\\meme.mp3")
+    
 if __name__=='__main__':
+    speak("call me if you want to use me and my name is jarvis")
+
+    while True:
+        
+        firstquery=first().lower()
+        if 'hey jarvis' or 'jarvis' in firstquery:
+            speak("hello ")
             wish()
+            # query=  takecommand().lower()
+            while True:
     
-            query=  takecommand().lower()
-            if 'wikipedia' in query:
-                query=query.replace("wikipedia", "")
-                res= wikipedia.summary(query,sentences=3)
-                speak("According to wikipedia")
-                speak(res)
-            elif 'open youtube' in query:
-                webbrowser.open('youtube.com')
-            elif 'open google' in query:
-                webbrowser.open('google.com')
-            elif 'open chrome' in query:
-                webbrowser.open('chrome.com')
-            elif 'open bizsouk' in query:
-                webbrowser.open('bizsouk.in')   
-            elif 'open stackoverflow' in query:
-                webbrowser.open('stackoverflow.com')
-            
-
-    
-            elif 'play music'  in query:
-                music_dir="C:\\Users\\vishwajith\\Music"
-                songs= os.listdir(music_dir)
-                print(songs)
-                if len(songs)==1:
-                    speak("You Dont have music to play")
-                    
-                else:  
-                    r=round(random.random()*100)
-                    try:
-                        os.startfile(os.path.join(music_dir,songs[r]))
-                    
-
-                    except Exception as e:
-                        os.startfile(os.path.join(music_dir,songs[round(random.random()*10)]))
-                    
-
-
-            elif 'time now' in query:
-                speak("time is ")
-                speak(datetime.datetime.now().hour)
-                speak(" ")
-                speak(datetime.datetime.now().minute)
+                query=  takecommand().lower()
+                if 'wikipedia' in query:
+                    query=query.replace("wikipedia", "")
+                    res= wikipedia.summary(query,sentences=3)
+                    speak("According to wikipedia")
+                    speak(res)
+                elif 'open youtube' in query:
+                    webbrowser.open('youtube.com')
+                elif 'open google' in query:
+                    webbrowser.open('google.com')
+                elif 'open chrome' in query:
+                    webbrowser.open('chrome.com')
+                elif 'open bizsouk' in query:
+                    webbrowser.open('bizsouk.in')   
+                elif 'open stackoverflow' in query:
+                    webbrowser.open('stackoverflow.com')
                 
-            
-            elif 'open vs code'  in query:
-                code_path="C:\\Users\\vishwajith\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-                os.startfile(code_path)
+
+        
+                elif 'play music'  in query:
+                    music_dir="C:\\Users\\vishwajith\\Music"
+                    songs= os.listdir(music_dir)
+                    print(songs)
+                    if len(songs)==1:
+                        speak("You Dont have music to play")
+                        
+                    else:  
+                        r=round(random.random()*100)
+                        try:
+                            os.startfile(os.path.join(music_dir,songs[r]))
+                        
+
+                        except Exception as e:
+                            os.startfile(os.path.join(music_dir,songs[round(random.random()*10)]))
+                        
+
+
+                elif 'time now' in query:
+                    speak("time is ")
+                    speak(datetime.datetime.now().hour)
+                    speak(" ")
+                    speak(datetime.datetime.now().minute)
+                    
                 
-            elif 'send email' in query:
-                speak("To who you want to send, Tell me receiver email id write down")
-                to=input()
-                speak("From which account you want to send tell me your email id write down")
-                ac=input()
-                speak("Tell me what to say")
-                content=takecommand()
-                sendemail(to,ac,content)
+                elif 'open vs code'  in query:
+                    code_path="C:\\Users\\vishwajith\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+                    os.startfile(code_path)
+                    
+                elif 'send email' in query:
+                    speak("To who you want to send, Tell me receiver email id write down")
+                    to=input()
+                    speak("From which account you want to send tell me your email id write down")
+                    ac=input()
+                    speak("Tell me what to say")
+                    content=takecommand()
+                    sendemail(to,ac,content)
+                
+                elif "bye" in query:
+                    speak("Thanks for using me, have a good day bye")
+                    
+                
+                else:
+                    meme()
     
 
                 
